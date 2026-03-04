cce27dd06ded4c3e4e297a01ac722ac422212c32
Loan69 <73693689+Loan69@users.noreply.github.com>
2026-03-04 17:10:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,8 @@
 
 # Corriger les permissions après montage du volume
 mkdir -p /data/.n8n
-chown -R node:node /data
-chmod -R 755 /data
+chmod -R 777 /data
 
 # Lancer n8n en tant que user node
-export N8N_USER_FOLDER=/data
-export HOME=/data
-export DATA_FOLDER=/data
+exec n8n start
 
-exec su-exec node n8n start 2>/dev/null || \
-     runuser -u node -- n8n start 2>/dev/null || \
-     n8n start
